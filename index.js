6f465263d8a3906bfb9bef289f93440e4f36b12c
Damien Butt <damien.butt@norgateav.com>
2022-04-30 17:03:30 +0100

    chore: initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const controlBox = document.getElementsByClassName("control-box")[0];
+const cbSlider = controlBox.querySelector("input.slider");
+const cbFill = controlBox.querySelector(".bar .fill");
+const cbValue = controlBox.querySelector(".value");
+
+function setBar() {
+    const min = parseInt(cbSlider.getAttribute("min"));
+    const max = parseInt(cbSlider.getAttribute("max"));
+    const value = parseInt(cbSlider.value);
+    const percent = ((value - min) / (max - min)) * 100;
+
+    cbFill.style.height = percent + "%";
+    cbValue.innerText = Math.round(percent) + "%";
+
+    if (percent > 0) {
+        controlBox.classList.add("on");
+    } else {
+        controlBox.classList.remove("on");
+    }
+
+    cbSlider.addEventListener("input", setBar);
+}
+
+setBar();
